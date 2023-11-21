900f98a342453ae8d73f3a57d2d8bff2da411505
Marina Tanaka <huttenloucher@gmail.com>
2023-11-21 11:58:20 -0300

    funcoes

--- 02-funcoes.js
+++ 02-funcoes.js
@@ -13,3 +13,22 @@ exemplo2();
 function exemplo2(){
     console.log("Função nomeada");
 };
+
+// Exercício para relembrar como utilizar os parametros
+// 1 - Criar uma função que receba valores numéricos, calcule a diferença entre eles e retorne o resultado.
+function soma (numero1, numero2){
+    let resultado = numero1 - numero2;
+    return resultado; 
+};
+
+// 2 - Chame a função duas vezes passando valores diferentes e guardando-os em variáveis de escopo global.
+let resultado1 = exercicio(10, 5);
+let resultado2 = exercicio(120, 66);
+let resultado3 = exercicio(10, 66)
+
+// 3 - Mostre os valores processados pela função no terminal
+console.log(resultado1);
+console.log(resultado2);
+
+// (resultado absoluto)
+console.log(Math.abs(resultado3) );
