6d16e55dc2ca8049b73399df185271532f6a676d
Marina Tanaka <huttenloucher@gmail.com>
2023-11-22 09:23:12 -0300

    exemplo de importação e exportação de modulos
    Obs, criado o package.json com type module pois o node não reconhece como padrão a sintaxe utilizada

---
+++ 04-codigo-modular.js
@@ -0,0 +1,4 @@
+// Importando o objeto pessoa
+import pessoa from './modulos/dados.js';
+
+console.log(pessoa);
---
+++ modulos/dados.js
@@ -0,0 +1,9 @@
+// Criando objeto com dados de uma pessoa
+const pessoa = {
+    nome: 'Marina Tanaka',
+    idade: 22,
+    cidade: 'São Paulo'
+};
+
+// Exportando como padrão o objeto pessoa
+export default pessoa;
