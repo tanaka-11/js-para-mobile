b9618111cb942bc031fe617907b7032cfba709cd
Marina Tanaka <huttenloucher@gmail.com>
2023-11-22 09:55:15 -0300

    Exercicio concluido

---
+++ exercicio-modulos.js
@@ -0,0 +1,6 @@
+import meses from "./modulos/meses.js";
+
+// Inves do for of como fiz um objeto inves do array tive que utilizar uma sintaxe diferente no loop utilizando o for in passando o objeto 'meses' e ai sim a chamada individualmente de cada mes atraves do 'mes' em colchetes
+for (const mes in meses) {
+    console.log(`${meses[mes]}`);
+}
---
+++ modulos/meses.js
@@ -0,0 +1,18 @@
+// Como utilizei as chaves para montar um objeto inves de um array obrigatoriamente tenho que utilizar o loop for in.
+
+const meses = {
+    primeiro: 'Janeiro',
+    segundo: 'Fevereiro',
+    terceiro: 'Março',
+    quarto: 'Abril',
+    quinto: 'Maio',
+    sexto: 'Junho',
+    setimo: 'Julho',
+    oitavo: 'Agosto',
+    nono: 'Setembro',
+    decimo: 'Outubro',
+    decimo_primeiro: 'Novembro',
+    decimo_segundo: 'Dezembro'
+};
+
+export default meses;
