5d224b4213d25f20e0fa9b3dcaea9717fdada09c
Marina Tanaka <huttenloucher@gmail.com>
2023-11-22 11:12:47 -0300

    importando as funções do modulo atraves' do as

--- 04-codigo-modular.js
+++ 04-codigo-modular.js
@@ -5,8 +5,11 @@ import {pessoa, livro, alunos as reprovados} from './modulos/dados.js';
 // Importando recursos de alunos.js
 import alunos from './modulos/alunos.js';
 
-// Importando recursos de funcoes.js
-import {converteMaiusculas, converteMinusculas, formataMoeda} from './modulos/funcoes.js'
+// Importando recursos de funcoes.js de maneira manual.
+// import {converteMaiusculas, converteMinusculas, formataMoeda} from './modulos/funcoes.js'
+
+// Importando recursos de funcoes.js com o 'as'.
+import * as minhasFuncoes from './modulos/funcoes.js';
 
 // Chamando dados do objeto pessoa
 console.log(pessoa);
@@ -27,15 +30,16 @@ console.log(reprovados);
 // Chamando dados do array alunos dentro do alunos.js utilizando o nome original do modulo 
 console.log(alunos);
 
-// Chamando função mesclado com outro modulo
-console.log(converteMaiusculas(reprovados[0]));
-console.log(converteMinusculas(alunos[0]));
+// Chamando função mesclada com outro modulo
+console.log(minhasFuncoes.converteMaiusculas(reprovados[0]));
+console.log(minhasFuncoes.converteMinusculas(alunos[0]));
 
-// Chamando função do modulo
+// Exemplo da função formataMoeda
 let preco1 = 1200.85;
 let preco2 = 100000.50;
 let preco3 = 500.8451;
 
-console.log(formataMoeda(preco1));
-console.log(formataMoeda(preco2));
-console.log(formataMoeda(preco3));
+// Chamando função formataMoeda do modulo
+console.log(minhasFuncoes.formataMoeda(preco1));
+console.log(minhasFuncoes.formataMoeda(preco2));
+console.log(minhasFuncoes.formataMoeda(preco3));
