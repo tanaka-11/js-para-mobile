237475a74e505ff89866ff94f862be6d5ebc82b0
Marina Tanaka <huttenloucher@gmail.com>
2023-11-22 10:28:28 -0300

    Update exercicio-modulos.js

--- exercicio-modulos.js
+++ exercicio-modulos.js
@@ -1,6 +1,7 @@
 import meses from "./modulos/meses.js";
 
 // Inves do for of como fiz um objeto inves do array tive que utilizar uma sintaxe diferente no loop utilizando o for in passando o objeto 'meses' e ai sim a chamada individualmente de cada mes atraves do 'mes' em colchetes
+
 // for (const mes in meses) {
 //     console.log(`${meses[mes]}`);
 // }
