17e1955d8e58902e53c7cd12cdba3e34a1f35e50
Marina Tanaka <huttenloucher@gmail.com>
2023-11-22 09:07:07 -0300

    exemplo de template literal

--- 03-template-literal.js
+++ 03-template-literal.js
@@ -4,13 +4,18 @@ let idade = 22;
 let cidade = 'São Paulo';
 let estado = 'SP';
 
-// Concatenação tradicional em JS utilizamos o operador '+'
-
 // Frase Exemplo:
 // Meu nome é Marina Tanaka tenho 22 anos e moro em São Paulo - SP. 
 
-// Obs. Utilizei \"\" por ter feito a mensagem com aspas simples inves de duplas, se utilizar a dupla a barra invertida não se torna necessaria e devemos utilizar a aspas simples.
+// Exemplo 1 - Concatenação tradicional em JS utilizamos o operador '+'
+// Obs. Utilizei \"\" por ter feito a mensagem com aspas simples inves de duplas, se utilizar a dupla a barra invertida não se torna necessaria e devemos utilizar as aspas simples.
 let mensagem1 = 'Meu nome é <b class=\"destaque\">'+nome+' '+sobrenome+'</b>, tenho '+idade+' anos e moro em '+cidade+' - '+estado+'.';
 
-// Exibindo a mensagem de exemplo
 console.log(mensagem1);
+
+
+// Exemplo 2 - Template Literal/String
+// Obs. Diferentemente da sintaxe tradicional no template literal utilizamos a crase e cifrão com chaves para concatenar
+let mensagem2 = `Meu nome é ${nome.toUpperCase()} ${sobrenome.toUpperCase()}, tenho ${idade} anos e moro em ${cidade} - ${estado}. Sou ${idade >= 18 ? 'maior' : 'menor'} de idade.`;
+
+console.log(mensagem2);
