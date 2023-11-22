a058ef37337a1425ac18f5062f88a26432db0025
Marina Tanaka <huttenloucher@gmail.com>
2023-11-22 09:39:15 -0300

    adicionado chamada de dados do objeto livro

--- 04-codigo-modular.js
+++ 04-codigo-modular.js
@@ -1,11 +1,15 @@
 // Importando o objeto pessoa
 import {pessoa, livro} from './modulos/dados.js';
 
-// Chamando dados
+// Chamando dados do objeto pessoa
 console.log(pessoa);
 console.log(pessoa.nome);
 
-// Chamando dados com loop
+// Chamando dados com loop do objeto pessoa
 for(const prop in pessoa) {
     console.log(pessoa[prop]);
 }
+
+// Chamando dados do objeto livro
+console.log(livro.titulo);
+console.log(livro.volume);
