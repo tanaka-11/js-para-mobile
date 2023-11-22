bacbe7c600c33f83b041b5e8e489fdb16598e682
Marina Tanaka <huttenloucher@gmail.com>
2023-11-22 09:36:57 -0300

    exemplo de como utilizar a sintaxe do export com mais de um recurso

--- 04-codigo-modular.js
+++ 04-codigo-modular.js
@@ -1,9 +1,11 @@
 // Importando o objeto pessoa
-import pessoa from './modulos/dados.js';
+import {pessoa, livro} from './modulos/dados.js';
 
+// Chamando dados
 console.log(pessoa);
 console.log(pessoa.nome);
 
+// Chamando dados com loop
 for(const prop in pessoa) {
     console.log(pessoa[prop]);
 }
--- modulos/dados.js
+++ modulos/dados.js
@@ -5,5 +5,15 @@ const pessoa = {
     cidade: 'São Paulo'
 };
 
-// Exportando como padrão o objeto pessoa
-export default pessoa;
+const livro = {
+    titulo: "Maldição do Titã",
+    volume: "Percy Jackson",
+    ano: 2013
+};
+
+// Exportando como padrão o objeto pessoa 
+// Obs. O default so funciona se eu precisar exportar apenas um recurso
+// export default pessoa;
+
+// Sintaxe quando vamos exportar mais de um recurso utilizando apenas a palavra-chave export e os recursos entre chaves
+export {pessoa, livro};
