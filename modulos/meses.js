4da102d2f8f14fed9a27aac751659ee892c69781
Marina Tanaka <huttenloucher@gmail.com>
2023-11-22 10:00:22 -0300

    Exercicio atualizado com as duas versões

--- modulos/meses.js
+++ modulos/meses.js
@@ -1,18 +1,34 @@
-// Como utilizei as chaves para montar um objeto inves de um array obrigatoriamente tenho que utilizar o loop for in.
 
-const meses = {
-    primeiro: 'Janeiro',
-    segundo: 'Fevereiro',
-    terceiro: 'Março',
-    quarto: 'Abril',
-    quinto: 'Maio',
-    sexto: 'Junho',
-    setimo: 'Julho',
-    oitavo: 'Agosto',
-    nono: 'Setembro',
-    decimo: 'Outubro',
-    decimo_primeiro: 'Novembro',
-    decimo_segundo: 'Dezembro'
-};
+// Versão com objeto (Utilizar com o loop for in)
+// const meses = {
+//     primeiro: 'Janeiro',
+//     segundo: 'Fevereiro',
+//     terceiro: 'Março',
+//     quarto: 'Abril',
+//     quinto: 'Maio',
+//     sexto: 'Junho',
+//     setimo: 'Julho',
+//     oitavo: 'Agosto',
+//     nono: 'Setembro',
+//     decimo: 'Outubro',
+//     decimo_primeiro: 'Novembro',
+//     decimo_segundo: 'Dezembro'
+// };
+
+// Versão com array (Utilizar com loop for of)
+const meses = [
+    'janeiro', 
+    'fevereiro', 
+    'março', 
+    'abril', 
+    'maio', 
+    'junho', 
+    'julho', 
+    'agosto', 
+    'setembro', 
+    'outubro', 
+    'novembro', 
+    'dezembro'
+];
 
 export default meses;
