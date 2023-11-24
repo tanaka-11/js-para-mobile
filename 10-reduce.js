368bca385963e53d8486e4d17c364b1b6676725c
Marina Tanaka <huttenloucher@gmail.com>
2023-11-24 11:42:25 -0300

    adicionado versões simplificadas

--- 10-reduce.js
+++ 10-reduce.js
@@ -4,9 +4,14 @@ import cursos from "./modulos/cursos.js";
 
 // Exemplo 1 Somando valores com reduce - Passamos primeiro um parametro de valores( recebendo o acumulador e o valor inicial) e como se trata de um acumulador precisamos colocar (, 0) apos a função ou (, 1) dependendo da operação matematica a ser feita.
 const valores = [10, 5, 50, 200, 1000];
-const totalValor = valores.reduce( (acumulador, valor) => {
-    return acumulador + valor
-}, 0);
+
+// Versão completa
+// const totalValor = valores.reduce( (acumulador, valor) => {
+//     return acumulador + valor
+// }, 0);
+
+// Versão simplificada
+const totalValor = valores.reduce( (acumulador, valor) => acumulador + valor, 0);
 
 console.log('Exemplo 1');
 console.log(valores);
@@ -31,20 +36,30 @@ console.log('Versão com Loop');
 console.log(acumuladora);
 
 // Exercicio 1 - Somando o preço dos cursos com reduce
-const totalCursos = cursos.reduce( (acumulador, curso) => {
-    return acumulador + curso.preco;
-}, 0);
+// Versão completa
+// const totalCursos = cursos.reduce( (acumulador, curso) => {
+//     return acumulador + curso.preco;
+// }, 0);
+
+// Versão simplificada
+const totalCursos = cursos.reduce( (acumulador, curso) => acumulador + curso.preco, 0);
 
 console.log('---');
 console.log('Exercicio 1');
 console.log(totalCursos);
 
 // Desafio
+
+// Versão completa
 const cursosFullStack = cursos.filter( curso => curso.categoria == 'Front-End' || curso.categoria == 'Back-End').reduce(
     (acumulador, curso) => {
         return acumulador + curso.preco;
     }, 0
 );
+
+// Versão simplificada
+// const cursosFullStack = cursos.filter( curso => curso.categoria == 'Front-End' || curso.categoria == 'Back-End').reduce(
+// (acumulador, curso) => acumulador + curso.preco , 0);
 
 console.log('---');
 console.log('Desafio');
