e6d3fc961a2bb62b127e967e1a6e20558aae95ff
Marina Tanaka <huttenloucher@gmail.com>
2023-11-28 09:12:12 -0300

    Criado uma função para guardar os dados de acesso da api

--- 11-promises-com-fetch.js
+++ 11-promises-com-fetch.js
@@ -3,26 +3,29 @@
 // Normalmente criamos uma constante para guardar a url da API
 const apiUrl =  `https://jsonplaceholder.typicode.com/users/1`;
 
-// Fetch(Conecte) - Primeiro se conectamos com a api 
-fetch(apiUrl)
+// Função de acesso da API
+function acessaAPI(){  
+    // Fetch(Conecte) - Primeiro se conectamos com a api 
+    fetch(apiUrl)
 
-// .then(Então) - Depois capturamos a resposta da API em formato JSON
-.then(resposta => {
-    // Se a resposta da requisição não for bem-sucedida
-    if(!resposta.ok){
-        throw new Error(`Erro na requisição: ${resposta.status} - ${resposta.statusText}`)
-    }
-    // Se estiver tudo okay, então retorne a resposta em formato json 
-    return resposta.json();
-})
+    // .then(Então) - Depois capturamos a resposta da API em formato JSON
+    .then(resposta => {
+        // Se a resposta da requisição não for bem-sucedida
+        if(!resposta.ok){
+            throw new Error(`Erro na requisição: ${resposta.status} - ${resposta.statusText}`)
+        }
+        // Se estiver tudo okay, então retorne a resposta em formato json 
+        return resposta.json();
+    })
 
-// .then(Então) - E então capturamos os dados contido na resposta
-.then(dados => console.log(dados) )
+    // .then(Então) - E então capturamos os dados contido na resposta
+    .then(dados => console.log(dados) )
 
-// .catch(Capture) - Por fim passamos o catch para capturar algum erro na aplicação
-.catch(error => {
-    console.error('Erro na operação:'+error.message)
-})
+    // .catch(Capture) - Por fim passamos o catch para capturar algum erro na aplicação
+    .catch(error => {
+        console.error('Erro na operação:'+error.message)
+    })
+}
 
-// Versão em 1 linha
-// fetch(apiUrl).then(resposta => resposta.json()).then(dados => console.log(dados)).catch(error => console.error('Erro na operação:'+error.message));
+// Executando a função de acesso
+acessaAPI();
