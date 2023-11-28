0a652f8d055657d6450f8047a4cf0defb2733ff3
Marina Tanaka <huttenloucher@gmail.com>
2023-11-28 11:09:36 -0300

    Versão 2 do exercicio (Bueno)

---
+++ exercicio-dom-api.js
@@ -0,0 +1,43 @@
+const apiUrl = `https://jsonplaceholder.typicode.com/users`;
+
+// Versão 2 diretamente do js
+function carregarDados(){ 
+    function acessaAPI() {
+        fetch(apiUrl)
+            .then(resposta => {
+                if (!resposta.ok) {
+                    throw new Error(
+                        `Erro na requisição: ${resposta.status} - ${resposta.statusText}`
+                    );
+                }
+                return resposta.json();
+            })
+            .then(dados => {
+                const usuario = document.getElementById('usuarios');
+
+                dados.forEach(usuarioAPI => {
+                    // Criando elementos HTML para cada usuário
+                    const hr = document.createElement('hr');
+                    const nome = document.createElement('h3');
+                    const email = document.createElement('p');
+                    const website = document.createElement('p');
+
+                    // Preenchendo os elementos com os dados do usuário
+                    nome.textContent = `Nome: ${usuarioAPI.name}`;
+                    email.textContent = `Email: ${usuarioAPI.email}`;
+                    website.textContent = `Website: ${usuarioAPI.website}`;
+
+                    // Adicionando os elementos criados à seção de usuários no HTML
+                    usuario.appendChild(hr); 
+                    usuario.appendChild(nome); 
+                    usuario.appendChild(email);
+                    usuario.appendChild(website); 
+                });
+            })
+            .catch(error => {
+                console.error("Erro na operação: " + error.message);
+            });
+    }
+    
+    acessaAPI();
+}
