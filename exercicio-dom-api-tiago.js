b0600dacb7348f12337a05d1cf64cc715b3b9488
Marina Tanaka <huttenloucher@gmail.com>
2023-11-28 11:42:29 -0300

    Resolução desafio tiago com map e desestruturação

--- exercicio-dom-api-tiago.js
+++ exercicio-dom-api-tiago.js
@@ -10,17 +10,18 @@ botao.addEventListener("click", async function(){
         const dados = await resposta.json();
         
         if(divUsuarios.innerHTML == ""){
-            for(const usuario of dados){
+            dados.map( ({name, email, website}) => {
                 let secao = document.createElement("section");
                 secao.innerHTML = `
-                    <h2>${usuario.name}</h2>
-                    <p>${usuario.email}</p>
-                    <p>${usuario.website}</p>
+                    <h2>${name}</h2>
+                    <p>${email}</p>
+                    <p>${website}</p>
                     <hr>
                 `;
                 divUsuarios.appendChild(secao);
-            }
+            })
         }
+
     } catch (error) {
         console.error(error.message);
     }
